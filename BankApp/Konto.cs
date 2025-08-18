8c5b60406c7cc892b25083d3bbfdd5720976fd76
Choaib Bourahla <Choaib.Bourahla@ksb-sg.ch>
2025-08-18 23:53:10 +0200

    Add project files.

---
+++ BankApp/Konto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    internal class Konto
+    {
+        private int kontonummer;
+        private string inhaber;
+        private decimal saldo = 0;
+
+        public Konto (int kontonummer, string inhaber, int saldo)
+        {
+            this.kontonummer = kontonummer;
+            this.inhaber = inhaber;
+            this.saldo = saldo;
+        }
+
+        public void Einzahlen(decimal betrag)
+        {
+            saldo += betrag;
+        }
+        public void Abheben(decimal betrag)
+        {
+            saldo -= betrag;
+        }
+
+        public void ZeigeInfo()
+        {
+            Console.WriteLine($"{kontonummer}\n {inhaber}\n {saldo}");
+        }
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Konto konto = new Konto(123456, "Bourahla", 0);
+            //bool status = true;
+            
+
+            /*if (antwort == "1")
+            {
+                Console.WriteLine("kontonummer: ");
+                int kontonummer1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("inhaber: ");
+
+                string inhaber1 = Console.ReadLine();
+
+                Konto konto1 = new Konto(kontonummer1, inhaber1, 0);
+            }*/
+            while (true)
+            {    
+                Console.WriteLine("1. neues konto eröffnen\n 2. geld einzahlen\n 3. geld abheben\n 4. konto info anzeigen\n 5. beenden");
+                string antwort = Console.ReadLine();
+                int Antwort = int.Parse(antwort);
+
+                if (Antwort == 2)
+                {
+                    Console.WriteLine("betrag: ");
+                    decimal betrag = decimal.Parse(Console.ReadLine());
+
+                    konto.Einzahlen(betrag);
+                    
+                }
+                else if (Antwort == 3)
+                {
+                    Console.WriteLine("betrag zum abheben: ");
+                    decimal betrag = decimal.Parse(Console.ReadLine());
+
+                    konto.Abheben(betrag);
+
+                }
+                else if (Antwort == 4)
+                {
+                    konto.ZeigeInfo();
+
+                }
+                else if (Antwort == 5)
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("ungültige eingabe");
+                }
+            }
+
+        }
+    }    
+        
+}
